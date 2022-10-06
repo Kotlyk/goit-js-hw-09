51212e52cba168679a117d9939c164f4f1f2083e
Kotlyk <irashakot@gmail.com>
2022-10-06 15:47:34 +0300

    update task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+console.log(startBtn);
+console.log(stopBtn);
+
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// };
+
+function onClickStart(getRandomHexColor) {
+  timerId = setInterval(() => {
+    document.body.style.background = `#${Math.floor(
+      Math.random() * 16777215
+    ).toString(16)}`;
+  }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function onClickStop() {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
