53e1eed2e917f3c2dc13c4ca70688b9ee58e0365
Kotlyk <104897730+Kotlyk@users.noreply.github.com>
2022-10-06 13:02:46 +0000

    Deploying to gh-pages from @ Kotlyk/goit-js-hw-09@6073ef977a54a9029630458ba81c094fd6847ee6 🚀

---
+++ 01-color-switcher.187453eb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");console.log(t),console.log(e),t.addEventListener("click",(function(o){n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}));var n=null}();
+//# sourceMappingURL=01-color-switcher.187453eb.js.map
---
+++ 01-color-switcher.6891315f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");console.log(t),console.log(e),t.addEventListener("click",(function(n){o=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}));let o=null;
+//# sourceMappingURL=01-color-switcher.6891315f.js.map
