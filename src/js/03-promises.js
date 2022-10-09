e5c64dba4a1583f856619d032bb3bfbd63d0bd0c
Kotlyk <irashakot@gmail.com>
2022-10-09 13:46:45 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+const submitBtn = document.querySelector('button')
+
+const firstDelay = document.querySelector('[name="delay"]');
+const delayStep = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+function submitCreatePromises(evt) {
+  evt.preventDefault();
+
+  let delay = firstDelay.valueAsNumber;
+  const delayStepVal = delayStep.valueAsNumber;
+  const amountVal = amount.valueAsNumber;
+
+  for (let i = 1; i <= amountVal; i+=1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+       console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += delayStepVal;
   }
 }
+
+submitBtn.addEventListener('click', submitCreatePromises);
